02ab1e5c0824ea8c68a457ab74337d5c0413b227
Elijah Iverson <eliiverson00@gmail.com>
2024-02-26 22:05:46 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # wordls-fetch
  
+"wordls-fetch" randomly fetches words from a wordlist like common.txt or rockyou.txt to give variety to crackable password in vulnerable environments.
+
+By default, it grabs one word randomly from the wordlist but using "-c" or "--count" you can get any unique entries from the wordlist as you like.
+
+## help page
+```
+usage: wordls-fetch.py [-h] [-c COUNT] [-o OUTPUT] filename
+
+fetches any number of words from a wordlist
+
+positional arguments:
+  filename              the name of a wordlist you want to fetch from
+
+options:
+  -h, --help            show this help message and exit
+  -c COUNT, --count COUNT
+                        the number of words you wish to fetch from the wordlist
+  -o OUTPUT, --output OUTPUT
+                        directs the output to a name of your choice
+```
---
+++ wordls-fetch.py
@@ -0,0 +1,37 @@
+import argparse
+from random import randrange
+
+parser = argparse.ArgumentParser(description='fetches any number of words from a wordlist')
+parser.add_argument('filename', help='the name of a wordlist you want to fetch from')
+parser.add_argument('-c','--count', type=int, default=1,
+                    help='the number of words you wish to fetch from the wordlist')
+parser.add_argument('-o','--output', help='directs the output to a name of your choice')
+args = parser.parse_args()
+
+try:
+    with open(args.filename, 'r') as data:
+        wordlist = [] 
+        while line := data.readline().replace('\n','').strip():
+            wordlist.append(str(line))
+    
+        if args.count > len(wordlist):
+            print(f"\n!! The wordlist you have entered doesn't have {args.count} entries therefore the max number of entries in the wordlist will be presented !!")
+            args.count = len(wordlist)
+            
+        if args.output is None:
+            print('')
+            for x in range(args.count):
+                idx = randrange(len(wordlist))
+                print(f"{x+1:03}. {wordlist[idx]}")
+                wordlist.pop(idx)
+            print('')
+        else:
+            with open(args.output, 'w') as output_file:
+                for x in range(args.count):
+                    idx = randrange(len(wordlist))
+                    output_file.write("%s\n" % wordlist[idx])
+                    wordlist.pop(idx)
+
+except Exception as err:
+    print("\n\n!! Something went wrong !!\n\n"
+          + f"{type(err)=}\n{err=}\n\n")
